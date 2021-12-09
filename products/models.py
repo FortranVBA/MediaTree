0231370751f23b23a609defa8b4d8b9cbc45b368
FortranVBA <mpyfabrice@gmail.com>
2021-12-09 14:39:44 +0100

    Product model creation

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
+"""Fruit Shop application - Products models file."""
+
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Product(models.Model):
+    """Product model."""
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
