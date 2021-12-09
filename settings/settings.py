d288dd4c69e4f6831959e303e6b7d8cdb81dc7fb
FortranVBA <mpyfabrice@gmail.com>
2021-12-09 14:39:06 +0100

    Template paths addition

--- settings/settings.py
+++ settings/settings.py
@@ -57,7 +57,12 @@ ROOT_URLCONF = "settings.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "settings", "templates"),
+            os.path.join(BASE_DIR, "products", "templates"),
+            os.path.join(BASE_DIR, "sells", "templates"),
+            os.path.join(BASE_DIR, "users", "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
