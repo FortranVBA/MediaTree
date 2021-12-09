b290a7bf5a0030da22f51ebf054a18b92c0d8002
FortranVBA <mpyfabrice@gmail.com>
2021-12-09 14:39:54 +0100

    Sell model creation

--- sells/models.py
+++ sells/models.py
@@ -1,3 +1,16 @@
+"""Fruit Shop application - Sells models file."""
+
 from django.db import models
+from products.models import Product
+from django.conf import settings
 
 # Create your models here.
+
+
+class Review(models.Model):
+    """Review model."""
+
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
