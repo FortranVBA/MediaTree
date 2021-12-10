64b344376e8998646370085db05328339e183d3b
FortranVBA <mpyfabrice@gmail.com>
2021-12-10 16:28:58 +0100

    Model name and fields update

--- sells/models.py
+++ sells/models.py
@@ -7,10 +7,12 @@ from django.conf import settings
 # Create your models here.
 
 
-class Review(models.Model):
-    """Review model."""
+class Sell(models.Model):
+    """Sell model."""
 
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     time_last_updated = models.DateTimeField(auto_now=True)
